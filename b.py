db04d0d5f55b699f06d164d93fa2a622e97efa4e
Your Name <hirenpatel1601@gmail.com>
2025-05-30 12:42:25 +0530

    first line is the subject line, followed by a blank line, and then the body of the commit message.

---
+++ b.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key="AIzaSyA0Wx86NmwHjWFoTUdnERgcioNlh78V-iE")
+
+
+def get_gemini_response(prompt):
+    model = genai.GenerativeModel('gemini-2.5-flash-preview-05-20')
+    response = model.generate_content(prompt)
+    return response.text
+
+
+st.title("The Professor's Academy Assistant")
+st.header("Ask me anything")
+
+userprompt = st.text_area("Enter your question here:", key="prompt")
+submitbtn = st.button("Get Answer")
+
+if submitbtn and userprompt.strip():
+    response = get_gemini_response(userprompt)
+    st.subheader("Your Answer:")
+    st.write(response)
